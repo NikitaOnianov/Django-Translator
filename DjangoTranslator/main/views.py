fe7e2ffcf31e4b52c07f03d4411d70f6e64e0e2c
NikitaOnianov <nikita.onianov@yandex.ru>
2024-05-17 11:35:31 +0300

    Почти готово

---
+++ DjangoTranslator/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from googletrans import Translator
+
+def index(request):
+    if request.method == 'POST':
+        lang = request.POST.get('lang', None)
+        txt = request.POST.get('text', None)
+
+        translator = Translator()
+        tr = translator.translate(txt, dest=lang)
+
+        return render(request, 'main/index.html', {'result': tr.text})
+
+    return render(request, 'main/index.html')
